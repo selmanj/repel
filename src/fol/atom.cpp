e0b359b43af134a356cd44d8939ba28e81fbf6c3
selmanj <selmanj@8fdccc8f-2872-4cc0-97d2-7131bebc01eb>
2011-04-12 20:17:18 +0000

    removed unnecessary file

--- src/fol/atom.cpp
+++
@@ -1,9 +0,0 @@
-#include "atom.h"
-#include <vector>
-#include <boost/shared_ptr.hpp>
-/*
-template <typename ForwardIterator>
-Atom::Atom(std::string name, ForwardIterator first, ForwardIterator last) 
-  : pred(name), terms(first, last) {
-}
-*/
