c718717d721c99befd39819006bd9805271b110d
selmanj <selmanj@8fdccc8f-2872-4cc0-97d2-7131bebc01eb>
2011-06-13 23:27:37 +0000

    partially written out, need to add an iterator to siset

--- src/fol/moves.cpp
+++ src/fol/moves.cpp
@@ -8,14 +8,35 @@
 #include "domain.h"
 #include "sentence.h"
 
-boost::optional<Moves> findMovesForLiquid(const Domain& d, const Model& m, const Sentence &s) {
-	return boost::optional<Moves>();
+Moves findMovesForLiquid(const Domain& d, const Model& m, const Sentence &s) {
+
 }
 
-boost::optional<Moves> findMovesForLiquidAtom(const Domain& d, const Model& m, const Atom &a) {
-	if (!a.isGrounded()) {
-		throw std::runtime_error("cannot handle atoms with variables at the moment!");
+Moves findMovesForLiquidLiteral(const Domain& d, const Model& m, const Sentence &s) {
+	if (dynamic_cast<const Atom*>(&s) || dynamic_cast<const Negation*>(&s)) {
+		const Atom* a;
+		bool isNegation=false;
+		if (dynamic_cast<const Atom*>(&s)) {
+			a = dynamic_cast<const Atom *>(&s);
+		} else {
+			const Negation* n = dynamic_cast<const Negation *>(&s);
+			isNegation = true;
+			a = dynamic_cast<const Atom *>(&(*n->sentence()));
+			if (!a) {
+				throw std::runtime_error("negation applied to something that is not an atom!");
+			}
+		}
+		if (!a->isGrounded()) {
+			throw std::runtime_error("cannot handle atoms with variables at the moment!");
+		}
+		// now that those checks are out of the way, find where it's not satisfied
+		SISet sat = d.satisfied(s, m);
+		sat = sat.compliment();	// compliment is where its not satisfied at
+		// Our moves are single span intervals, so split them out into a list of moves
+
+
 	}
+
 	// find where it's satisfied
 
 }
--- src/fol/moves.h
+++ src/fol/moves.h
@@ -13,14 +13,14 @@ class LiquidOp;
 class Sentence;
 
 struct Moves {
-	typedef std::pair <Atom, SISet> move;
+	typedef std::pair <Atom, SpanInterval> move;
 	std::vector<move> toAdd;
 	std::vector<move> toDel;
 };
 
 namespace {
-	boost::optional<Moves> findMovesForLiquid(const Domain& d, const Model& m, const Sentence &s);
-	boost::optional<Moves> findMovesForLiquidAtom(const Domain& d, const Model& m, const Atom &a);
+	Moves findMovesForLiquid(const Domain& d, const Model& m, const Sentence &s);
+	Moves findMovesForLiquidLiteral(const Domain& d, const Model& m, const Sentence &s);
 
 }
 
