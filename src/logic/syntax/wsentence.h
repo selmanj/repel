b0b57733e6952071ad141dc069708a9dfb15d4e8
Joseph Selman <selman.joe@gmail.com>
2011-12-08 14:39:11 -0800

    changed sentence() to return a copy rather than a const reference

--- src/logic/syntax/wsentence.h
+++ src/logic/syntax/wsentence.h
@@ -18,8 +18,8 @@ public:
 	virtual ~WSentence() {};
 
 	bool operator==(const WSentence& b) const {return (*s_ == *b.s_ && w_ == b.w_);};
-	const boost::shared_ptr<Sentence>& sentence() { return s_;}
-	boost::shared_ptr<const Sentence> sentence() const {return s_;}
+	boost::shared_ptr<Sentence> sentence() { return s_;}
+	const boost::shared_ptr<const Sentence> sentence() const {return s_;}
 	unsigned int weight() const {return w_;};
 
 	void setSentence(const boost::shared_ptr<Sentence>& s) {s_ = s;};
