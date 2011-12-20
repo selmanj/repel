e9aaccc98e66395382a1dd36b8694752f9a5c291
Joseph Selman <selman.joe@gmail.com>
2011-12-19 16:19:19 -0800

    cleaned up constant, removed constant.cpp (unneeded for a simple class)

--- src/logic/syntax/constant.h
+++ src/logic/syntax/constant.h
@@ -7,20 +7,43 @@
 class Constant : public Term {
 public:
 	Constant(std::string name);
-	Constant(const Constant& c) : name_(c.name_) {};
-	~Constant() {};
+	Constant(const Constant& c);
+	~Constant();
 
-	void operator=(const Constant& c) {name_ = c.name_;};
-
+	Constant& operator=(const Constant& other);
 protected:
-	virtual void doToString(std::string& str) const {
-		str += name_;
-	}
+	virtual void doToString(std::string& str) const;
 private:
-	virtual Term* doClone() const;
-	virtual std::string doName() const {return name_;};
-	virtual bool doEquals(const Term& t) const;
 	std::string name_;
+
+	virtual Term* doClone() const;
+	virtual std::string doName() const;
+	virtual bool doEquals(const Term& t) const;
 };
+
+// implementation
+inline Constant::Constant(std::string name) : name_(name) {}
+inline Constant::Constant(const Constant& c) : name_(c.name_) {}
+inline Constant::~Constant() {}
+
+inline Constant& Constant::operator=(const Constant& other) { if (this != &other) name_ = other.name_;}
+
+// protected members
+inline void Constant::doToString(std::string& str) const {str += name_;}
+
+// private members
+inline Term* Constant::doClone() const {return new Constant(*this);}
+
+// TODO: what the hell is doname???
+inline std::string Constant::doName() const {return name_;}
+inline bool Constant::doEquals(const Term& t) const {
+	// first try to cast t to a constant
+	const Constant *con = dynamic_cast<const Constant*>(&t);
+	if (con == NULL) {
+		return false; // wrong type
+	}
+	return con->name_ == name_;
+}
+
 
 #endif
