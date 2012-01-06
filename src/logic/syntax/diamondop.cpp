b87ad539575f02c66b8a5fea89c053a7bbaef72a
Joseph Selman <selman.joe@gmail.com>
2012-01-06 13:38:34 -0800

    cleaned up diamondop a bit before i got started

---
+++ src/logic/syntax/diamondop.cpp
@@ -0,0 +1,46 @@
+#include "diamondop.h"
+
+const std::set<Interval::INTERVAL_RELATION>& DiamondOp::defaultRelations() {
+	static std::set<Interval::INTERVAL_RELATION>* defaults = new std::set<Interval::INTERVAL_RELATION>();
+	if (defaults->size() == 0) {
+		defaults->insert(Interval::STARTS);
+		defaults->insert(Interval::STARTSI);
+		defaults->insert(Interval::DURING);
+		defaults->insert(Interval::DURINGI);
+		defaults->insert(Interval::FINISHES);
+		defaults->insert(Interval::FINISHESI);
+		defaults->insert(Interval::OVERLAPS);
+		defaults->insert(Interval::OVERLAPSI);
+	}
+	return *defaults;
+}
+
+void DiamondOp::doToString(std::stringstream& str) const {
+	str << "<>";
+	if (rels_ != DiamondOp::defaultRelations()) {
+		str << "{";
+		if (!rels_.empty())	{	// better be safe, not sure if this could happen but still...
+			std::set<Interval::INTERVAL_RELATION>::const_iterator it = rels_.begin();
+			str << Interval::relationToString(*it);
+			it++;
+			for (; it!= rels_.end(); it++) {
+				str << ", ";
+				str << Interval::relationToString(*it);
+			}
+		}
+
+		str << "}";
+	}
+	str << " ";
+
+	if (s_ != NULL) {
+		// check if we need parenthesis
+		if (s_->precedence() > precedence()) {
+			str << "(";
+			str << s_->toString();
+			str << ")";
+		} else {
+			str << s_->toString();
+		}
+	}
+};
--- src/logic/syntax/diamondop.h
+++ src/logic/syntax/diamondop.h
@@ -5,93 +5,81 @@
 #include <set>
 #include "sentence.h"
 #include "sentencevisitor.h"
+#include "../../interval.h"
 
 class DiamondOp : public Sentence {
 public:
-	DiamondOp(boost::shared_ptr<Sentence> sentence)
-		: rels_(), s_(sentence) {
-			rels_ = std::set<Interval::INTERVAL_RELATION>(DiamondOp::defaultRelations().begin(), DiamondOp::defaultRelations().end());
-	}
+	static const std::set<Interval::INTERVAL_RELATION>& defaultRelations();
+
+	DiamondOp(boost::shared_ptr<Sentence> sentence);
 	DiamondOp(boost::shared_ptr<Sentence> sentence,
-			Interval::INTERVAL_RELATION relation)
-		: rels_(), s_(sentence) {
-		rels_.insert(relation);
-	}
+			Interval::INTERVAL_RELATION relation);
 	template <class InputIterator>
 	DiamondOp(boost::shared_ptr<Sentence> sentence,
 			InputIterator begin,
-			InputIterator end)
-			: rels_(begin, end), s_(sentence) {};
-	DiamondOp(const DiamondOp& dia) : rels_(dia.rels_), s_(dia.s_) {}; // shallow copy
-	virtual ~DiamondOp() {};
+			InputIterator end);
+	DiamondOp(const DiamondOp& dia); // shallow copy
+	virtual ~DiamondOp();
 
-	boost::shared_ptr<Sentence>& sentence() {return s_;};
-	boost::shared_ptr<const Sentence> sentence() const {return s_;};
-	const std::set<Interval::INTERVAL_RELATION>& relations() const {return rels_;};
+	boost::shared_ptr<Sentence>& sentence();
+	boost::shared_ptr<const Sentence> sentence() const;
+	const std::set<Interval::INTERVAL_RELATION>& relations() const;
 
-	static const std::set<Interval::INTERVAL_RELATION>& defaultRelations() {
-		static std::set<Interval::INTERVAL_RELATION>* defaults = new std::set<Interval::INTERVAL_RELATION>();
-		if (defaults->size() == 0) {
-			defaults->insert(Interval::STARTS);
-			defaults->insert(Interval::STARTSI);
-			defaults->insert(Interval::DURING);
-			defaults->insert(Interval::DURINGI);
-			defaults->insert(Interval::FINISHES);
-			defaults->insert(Interval::FINISHESI);
-			defaults->insert(Interval::OVERLAPS);
-			defaults->insert(Interval::OVERLAPSI);
-		}
-		return *defaults;
-	}
 private:
 	std::set<Interval::INTERVAL_RELATION> rels_;
 	boost::shared_ptr<Sentence> s_;
 
-	virtual Sentence* doClone() const { return new DiamondOp(*this); };
-	virtual bool doEquals(const Sentence& s) const {
-		const DiamondOp *dia = dynamic_cast<const DiamondOp*>(&s);
-		if (dia == NULL) {
-			return false;
-		}
-		return *s_ == *(dia->s_);
-	};
+	virtual Sentence* doClone() const;
+	virtual bool doEquals(const Sentence& s) const;
 
-	virtual void doToString(std::stringstream& str) const {
-		str << "<>";
-		if (rels_ != DiamondOp::defaultRelations()) {
-			str << "{";
-			if (!rels_.empty())	{	// better be safe, not sure if this could happen but still...
-				std::set<Interval::INTERVAL_RELATION>::const_iterator it = rels_.begin();
-				str << Interval::relationToString(*it);
-				it++;
-				for (; it!= rels_.end(); it++) {
-					str << ", ";
-					str << Interval::relationToString(*it);
-				}
-			}
+	virtual void doToString(std::stringstream& str) const;
 
-			str << "}";
-		}
-		str << " ";
-
-		if (s_ != NULL) {
-			// check if we need parenthesis
-			if (s_->precedence() > precedence()) {
-				str << "(";
-				str << s_->toString();
-				str << ")";
-			} else {
-				str << s_->toString();
-			}
-		}
-	};
-
-	virtual int doPrecedence() const { return 2; };
-	virtual void visit(SentenceVisitor& v) const {
-		s_->visit(v);
-
-		v.accept(*this);
-	}
+	virtual int doPrecedence() const;
+	virtual void visit(SentenceVisitor& v) const;
 };
 
+// implementation below
+
+
+
+// constructors
+inline DiamondOp::DiamondOp(boost::shared_ptr<Sentence> sentence)
+	: rels_(), s_(sentence) {
+		rels_ = std::set<Interval::INTERVAL_RELATION>(DiamondOp::defaultRelations().begin(), DiamondOp::defaultRelations().end());
+}
+inline DiamondOp::DiamondOp(boost::shared_ptr<Sentence> sentence,
+		Interval::INTERVAL_RELATION relation)
+	: rels_(), s_(sentence) {
+	rels_.insert(relation);
+}
+template <class InputIterator>
+inline DiamondOp::DiamondOp(boost::shared_ptr<Sentence> sentence,
+		InputIterator begin,
+		InputIterator end)
+		: rels_(begin, end), s_(sentence) {}
+// copy constructor
+inline DiamondOp::DiamondOp(const DiamondOp& dia)
+	: rels_(dia.rels_), s_(dia.s_) {}; // shallow copy
+inline DiamondOp::~DiamondOp() {}
+
+// public methods
+inline boost::shared_ptr<Sentence>& DiamondOp::sentence() {return s_;}
+inline boost::shared_ptr<const Sentence> DiamondOp::sentence() const {return s_;}
+inline const std::set<Interval::INTERVAL_RELATION>& DiamondOp::relations() const {return rels_;}
+
+// private methods
+inline Sentence* DiamondOp::doClone() const { return new DiamondOp(*this); }
+inline bool DiamondOp::doEquals(const Sentence& s) const {
+	const DiamondOp *dia = dynamic_cast<const DiamondOp*>(&s);
+	if (dia == NULL) {
+		return false;
+	}
+	return *s_ == *(dia->s_);
+}
+inline int DiamondOp::doPrecedence() const { return 2; };
+inline void DiamondOp::visit(SentenceVisitor& v) const {
+	s_->visit(v);
+
+	v.accept(*this);
+}
 #endif
