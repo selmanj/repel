e0be1ada9d2d9b05d2f217937f5384525f7a865f
Joe Selman <selman.joe@gmail.com>
2012-02-06 11:48:20 -0800

    term refactored so it's easier to read

--- src/logic/syntax/term.h
+++ src/logic/syntax/term.h
@@ -7,17 +7,14 @@
 
 class Term : boost::noncopyable {
 public:
-    virtual ~Term() {};
-    std::string name() const { return doName(); };
-    Term* clone() const { return doClone(); };
-    bool operator==(const Term& b) const {
-        return doEquals(b);};
+    virtual ~Term();
 
-    std::string toString() const {
-        std::string str;
-        doToString(str);
-        return str;
-    };
+    std::string name() const;
+    Term* clone() const;
+    friend bool operator==(const Term& a, const Term& b);
+    friend bool operator!=(const Term& a, const Term& b);
+
+    std::string toString() const;
 
 protected:
     virtual void doToString(std::string& str) const = 0;
@@ -28,7 +25,21 @@ private:
     virtual bool doEquals(const Term& t) const = 0;
 };
 
-inline Term* new_clone(const Term& t) {
-    return t.clone();
+// IMPLEMENTATION
+inline Term::~Term() {};
+
+inline std::string Term::name() const { return doName(); }
+inline Term* Term::clone() const { return doClone(); }
+
+inline bool operator==(const Term& a, const Term& b) { return a.doEquals(b);}
+inline bool operator!=(const Term& a, const Term& b) { return !operator==(a,b);}
+
+inline std::string Term::toString() const {
+    std::string str;
+    doToString(str);
+    return str;
 }
+
+inline Term* new_clone(const Term& t) {return t.clone();}
+
 #endif
