b746e084a7404df86eab9b5195091b3e050cbd4f
Joe Selman <selman.joe@gmail.com>
2012-03-07 16:18:08 -0800

    little bit faster code for predcollector (although it should be called atomcollector

--- src/logic/predcollector.h
+++ src/logic/predcollector.h
@@ -1,6 +1,7 @@
 #ifndef PREDCOLLECTOR_H
 #define PREDCOLLECTOR_H
 
+#include <boost/unordered_set.hpp>
 #include "syntax/sentencevisitor.h"
 #include "syntax/atom.h"
 
@@ -9,14 +10,14 @@ public:
     PredCollector() {};
     virtual ~PredCollector() {};
     virtual void accept(const Sentence& s) {
-        // only care about atoms
-        const Atom* a = dynamic_cast<const Atom*>(&s);
-        if (a != NULL) {
-            preds.insert(*a);
+        // only care about predicates
+        // TODO: this should collect types, not atoms
+        if (s.getTypeCode() == Atom::TypeCode) {
+            preds.insert(static_cast<const Atom>(s));
         }
     }
 
-    std::set<Atom, atomcmp> preds;
+    boost::unordered_set<Atom> preds;
 };
 
 #endif
