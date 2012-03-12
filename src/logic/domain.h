b0e364fd4b7aed421d7593baa73ff36571ef4bf9
Joe Selman <selman.joe@gmail.com>
2012-03-12 14:36:25 -0700

    cleaned up domain file

--- src/logic/domain.h
+++ src/logic/domain.h
@@ -28,8 +28,6 @@
 
 std::string modelToString(const Model& m);
 
-// TODO: this is quite a bad way to handle closed worlds.  Redo this entire mechanic so model's account for this, not the domain.
-
 class Domain {
 public:
     typedef boost::unordered_map<Proposition, SISet> PropMap;
@@ -37,19 +35,8 @@ public:
     typedef std::vector<ELSentence>::const_iterator     formula_const_iterator;
     typedef PropMap::const_iterator                     fact_const_iterator;
     typedef boost::unordered_set<Atom>::const_iterator  atom_const_iterator;
-    //typedef boost::unordered_set<Atom>::const_iterator obsAtoms_const_iterator;
-    //typedef boost::unordered_set<Atom>::const_iterator unobsAtoms_const_iterator;
-    //typedef MergedIterator<obsAtoms_const_iterator, unobsAtoms_const_iterator, const Atom> atoms_const_iterator;
 
     Domain();
-    /*
-    template <class FactsForwardIterator>
-    Domain(FactsForwardIterator factsBegin, FactsForwardIterator factsEnd,
-            const std::vector<ELSentence>& formSet,
-            bool assumeClosedWorld=true);
-    virtual ~Domain();
-    //const FormulaList& formulas() const;
-     */
 
     formula_const_iterator formulas_begin() const;
     formula_const_iterator formulas_end() const;
@@ -60,13 +47,6 @@ public:
 
     std::size_t atoms_size() const;
 
-    //obsAtoms_const_iterator obsAtoms_begin() const;
-    //obsAtoms_const_iterator obsAtoms_end() const;
-    //unobsAtoms_const_iterator unobsAtoms_begin() const;
-    //unobsAtoms_const_iterator unobsAtoms_end() const;
-    //atoms_const_iterator atoms_begin() const;
-    //atoms_const_iterator atoms_end() const;
-
     void clearFormulas();
     void clearFacts();
     void addFormula(const ELSentence& e);
@@ -75,11 +55,8 @@ public:
     void addFact(const ELSentence& e);
     void addFact(const Proposition& p, const SISet& where);
 
-    //void addObservedPredicate(const Atom& a);
-    //const std::map<std::string, SISet>& observedPredicates() const; // TODO RENAME
     SISet getModifiableSISet(const Atom& a) const;
     SISet getModifiableSISet(const Atom& a, const SISet& where) const;
-    //void unsetAtomAt(const std::string& name, const SISet& where);
 
     NameGenerator& nameGenerator();
     Model defaultModel() const;
@@ -90,9 +67,7 @@ public:
 
     bool isLiquid(const std::string& predicate) const;
     bool dontModifyObsPreds() const;
-    //bool assumeClosedWorld() const;
     void setDontModifyObsPreds(bool b);
-    //void setAssumeClosedWorld(bool b);
 
     score_t score(const ELSentence& s, const Model& m) const;
     score_t score(const Model& m) const;
@@ -102,34 +77,14 @@ private:
 
     void growMaxInterval(const Interval& maxInterval);
 
-    //typedef std::pair<const Model*, const Sentence*> ModelSentencePair;
-
-   // bool assumeClosedWorld_;
     bool dontModifyObsPreds_;
-
-    //std::map<std::string, SISet> obsPredsFixedAt_;
-    //boost::unordered_set<Atom> obsPreds_;
-    //boost::unordered_set<Atom> unobsPreds_;
-
     Interval maxInterval_;
     std::vector<ELSentence> formulas_;
     PropMap partialModel_;
     boost::unordered_set<PredicateType> predTypes_;
     boost::unordered_set<Atom> allAtoms_;
-    //Model observations_;
 
     NameGenerator generator_;
-
-    /*
-    struct ModelSentencePair_cmp : public std::binary_function<ModelSentencePair, ModelSentencePair, bool> {
-        bool operator() (const ModelSentencePair& a, const ModelSentencePair& b) const {
-            if (a.first < b.first) return true;
-            if (a.first > b.first) return false;
-            if (a.second < b.second) return true;
-            return false;
-        }
-    };
-    */
 
     //mutable LRUCache<ModelSentencePair,SISet,ModelSentencePair_cmp> cache_;
 };
@@ -143,105 +98,6 @@ inline Domain::Domain()
       predTypes_(),
       allAtoms_(),
       generator_(){};
-/*
-template <class FactsForwardIterator>
-Domain::Domain(FactsForwardIterator factsBegin, FactsForwardIterator factsEnd,
-        const std::vector<ELSentence>& formSet,
-        bool assumeClosedWorld)
-        : assumeClosedWorld_(assumeClosedWorld),
-          dontModifyObsPreds_(true),
-          obsPredsFixedAt_(),
-          obsPreds_(),
-          unobsPreds_(),
-          maxInterval_(0,0),
-          formulas_(formSet),
-          observations_(),
-          generator_() {
-
-    // find the maximum interval of time
-    if (factsBegin == factsEnd) {
-        // what to do for time??
-        std::runtime_error e("no facts given: currently need at least one fact to determine the interval to reason over!");
-        throw e;
-    }
-    unsigned int smallest=std::numeric_limits<unsigned int>::max(), largest=std::numeric_limits<unsigned int>::min();
-    for (FactsForwardIterator it = factsBegin; it != factsEnd; it++) {
-        SpanInterval interval = it->where();
-
-        boost::optional<SpanInterval> norm = interval.normalize();
-        if (!norm) {
-            continue;
-        }
-        interval = norm.get();
-        smallest = (std::min)(interval.start().start(), smallest);
-        largest = (std::max)(interval.finish().finish(), largest);
-    }
-    for (std::vector<ELSentence>::const_iterator it = formulas_.begin(); it != formulas_.end(); it++) {
-        if (!it->isQuantified()) continue;
-        SISet set = it->quantification();
-        for (SISet::const_iterator it2 = set.begin(); it2 != set.end(); it2++) {
-            smallest = (std::min)(it2->start().start(), smallest);
-            largest = (std::max)(it2->finish().finish(), largest);
-        }
-    }
-    maxInterval_ = Interval(smallest, largest);
-
-    // collect all fact predicates
-    for (FactsForwardIterator it = factsBegin; it != factsEnd; it++) {
-        boost::shared_ptr<const Atom> a = it->atom();
-        SpanInterval si = it->where();
-
-        if (obsPredsFixedAt_.find(a->name()) == obsPredsFixedAt_.end()) {
-            SISet newSet(true, maxInterval_);       // TODO: assumes all unobs are liquid!!!
-            newSet.add(si);
-            obsPredsFixedAt_.insert(std::pair<std::string, SISet>(a->name(), newSet));
-
-            // add it as an observed predicate
-            obsPreds_.insert(*a);
-        } else {
-            SISet curSet = obsPredsFixedAt_.find(a->name())->second;
-            curSet.add(si);
-            obsPredsFixedAt_.erase(a->name());
-            obsPredsFixedAt_.insert(std::pair<std::string, SISet>(a->name(), curSet));
-        }
-    }
-    // now collect all unobserved preds
-
-    PredCollector predCollector;
-    for (std::vector<ELSentence>::const_iterator it = formulas_.begin(); it != formulas_.end(); it++) {
-        it->sentence()->visit(predCollector);
-    }
-
-    for (std::set<Atom, atomcmp>::const_iterator it = predCollector.preds.begin(); it != predCollector.preds.end(); it++) {
-        if (obsPreds_.find(*it) == obsPreds_.end()) {
-            unobsPreds_.insert(*it);
-        }
-    }
-
-    // initialize observations
-    for (FactsForwardIterator it = factsBegin; it != factsEnd; it++) {
-        boost::shared_ptr<const Atom> atom = it->atom();
-        SpanInterval interval = it->where();
-
-        // TODO: we are hardwired for liquidity, come back and fix this later
-
-        if (it->truthVal()) {
-            SISet set(true, maxInterval_);
-
-            set.add(interval);
-            observations_.setAtom(*atom, set);
-        }
-    }
-
-    // enforce maxInterval on our formulas
-    for(std::vector<ELSentence>::iterator it = formulas_.begin(); it != formulas_.end(); it++) {
-        if (!it->isQuantified()) continue;
-        SISet set = it->quantification();
-        set.setMaxInterval(maxInterval_);
-        it->setQuantification(set);
-    }
-};
-*/
 
 inline Domain::formula_const_iterator Domain::formulas_begin() const {return formulas_.begin();}
 inline Domain::formula_const_iterator Domain::formulas_end() const {return formulas_.end();}
