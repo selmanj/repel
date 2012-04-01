035f6e18b1358a7131cad8cada905e889690ad3d
Joe Selman <selman.joe@gmail.com>
2012-04-01 14:44:11 -0700

    Added structure for util directory, including a tool to create volleyball data files

---
+++ util/volleyball-gen.cpp
@@ -0,0 +1,13 @@
+/*
+ * volleyball-gen.cpp
+ *
+ *  Created on: Apr 1, 2012
+ *      Author: selman.joe@gmail.com
+ */
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, world." << std::endl;
+}
+
+
