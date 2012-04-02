487921fe64f0680cb51e21b77b9717fb8379ad6b
Joe Selman <selman.joe@gmail.com>
2012-04-02 07:43:27 -0700

    more structure

---
+++ util/volleyball_generator.cpp
@@ -0,0 +1,8 @@
+/*
+ * volleyball_generator.cpp
+ *
+ *  Created on: Apr 1, 2012
+ *      Author: selman.joe@gmail.com
+ */
+
+#include "volleyball_generator.h"
---
+++ util/volleyball_generator.h
@@ -0,0 +1,20 @@
+/*
+ * volleyball_generator.h
+ *
+ *  Created on: Apr 1, 2012
+ *      Author: selman.joe@gmail.com
+ */
+
+#ifndef VOLLEYBALL_GENERATOR_H_
+#define VOLLEYBALL_GENERATOR_H_
+
+class VolleyballGenerator {
+public:
+
+private:
+};
+
+
+
+
+#endif /* VOLLEYBALL_GENERATOR_H_ */
