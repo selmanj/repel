5d43836928a88dffa088cf185cc9654341a89f3f
Joe Selman <selman.joe@gmail.com>
2012-05-15 11:36:51 -0700

    refactors made to FOLToken

--- src/logic/FOLToken.cpp
+++ src/logic/FOLToken.cpp
@@ -1,9 +1,4 @@
 
-#include <string>
 #include "FOLToken.h"
 
-FOLToken::FOLToken(FOLParse::TokenType type, std::string contents) 
-: typ(type), data(contents)
-{
-}
-
+// This file intentionally left empty.
--- src/logic/FOLToken.h
+++ src/logic/FOLToken.h
@@ -3,7 +3,12 @@
 
 #include <string>
 
+// TODO: rename FOLParse and all its ilk to ELParse (event logic parse)
 namespace FOLParse {
+/**
+ * TokenType represents a type of token when parsing event logic.  Each token
+ * corresponds to some ASCII representation.
+ */
 enum TokenType { Invalid,
     OpenParen,
     CloseParen,
@@ -39,18 +44,58 @@ enum TokenType { Invalid,
     Infinity};
 }
 
+/**
+ * A class representing a token resulting from running the lexer on an input
+ * stream.  Tokens assist when parsing (and also contain some information
+ * about the parse).
+ */
 class FOLToken {
 public:
-    FOLToken(FOLParse::TokenType type=FOLParse::Invalid, std::string contents="");
+    /**
+     * Construct a FOLToken of the given type and contents.  By default
+     * (with no args) constructs an invalid token with no contents.
+     *
+     * @param type  The type of token.
+     * @param contents  The contents of the token
+     */
+    FOLToken(FOLParse::TokenType type=FOLParse::Invalid, const std::string& contents="")
+        : type_(type), contents_(contents) {}
 
-    FOLParse::TokenType type() const {return typ;};
-    void setType(FOLParse::TokenType type) {typ=type;};
-    std::string contents() const {return data;};
-    void setContents(std::string contents) {data = contents;};
+    /**
+     * Get the type of the token.
+     *
+     * @return the type of the token.
+     */
+    FOLParse::TokenType type() const {return type_;};
+
+    /**
+     * Get the contents of the token.  The contents are an ASCII representation
+     * of the token (ie a token of type comma has contents ",").  If the token
+     * type is a number or identifier, the contents are a string representation
+     * of the identifier or number.
+     *
+     * @return a string representation of the token type.
+     */
+    std::string contents() const {return contents_;};
+
+    /**
+     * Set the type of the token.
+     *
+     * @param type the new type of the token
+     */
+    void setType(FOLParse::TokenType type) {type_=type;};
+
+    /**
+     * Set the contents of the token.  This is an ASCII representation of the
+     * type.
+     *
+     * @param contents The contents of the token.
+     */
+    void setContents(const std::string& contents) {contents_ = contents;};
 
 private:
-    FOLParse::TokenType typ;
-    std::string data;
+    FOLParse::TokenType type_;
+    std::string contents_;
 };
 
 #endif
