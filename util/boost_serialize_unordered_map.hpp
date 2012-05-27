2e42925285a1e9080b9800a2fdff4fe1d0d72b2e
Joseph Selman <selman.joe@gmail.com>
2012-05-27 15:15:00 -0700

    better code for serialization using make_nvp where necessary

--- util/boost_serialize_unordered_map.hpp
+++ util/boost_serialize_unordered_map.hpp
@@ -11,6 +11,7 @@
 #include <boost/unordered_map.hpp>
 #include <boost/serialization/map.hpp>
 #include <boost/serialization/split_free.hpp>
+#include <boost/serialization/nvp.hpp>
 
 namespace boost { namespace serialization {
 template<class Archive, typename T, typename U>
@@ -30,9 +31,11 @@ void save(Archive & ar, const boost::unordered_map<T,U> & t, unsigned int versio
     typedef typename boost::unordered_map<T, U>::size_type size_type;
     typedef typename boost::unordered_map<T,U>::const_iterator const_iterator;
     size_type size = t.size();
-    ar & size;
+    ar & BOOST_SERIALIZATION_NVP(size);
+    unsigned int count = 0;
     for (const_iterator it = t.begin(); it != t.end(); it++) {
-        ar & *it;
+        ar & boost::serialization::make_nvp("pair" + count, *it);
+        count++;
     }
 }
 
@@ -44,11 +47,11 @@ void load(Archive & ar, boost::unordered_map<T,U> & t, unsigned int version)
     // read the size
     typedef typename boost::unordered_map<T,U>::size_type size_type;
     size_type size;
-    ar & size;
+    ar & BOOST_SERIALIZATION_NVP(size);
     for (size_type i = 0; i < size; i++) {
         // read a pair
         std::pair<T, U> pair;
-        ar & pair;
+        ar & boost::serialization::make_nvp("pair"+i, pair);
         // insert it into the map
         t.insert(pair);
     }
