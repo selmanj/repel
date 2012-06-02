fd6b150c36a089a07e59e9f38fdaf5d4a50422cd
Joe Selman <selman.joe@gmail.com>
2012-06-02 15:29:21 -0700

    added initial structure of main file for running experiments - nothing there yet

---
+++ util/volleyball-mcsat.cpp
@@ -0,0 +1,15 @@
+/*
+ * volleyball-mcsat.cpp
+ *
+ *  Created on: Jun 1, 2012
+ *      Author: selman.joe@gmail.com
+ */
+#include <boost/random.hpp>
+#include <stdexcept>
+#include "../src/logic/ELSyntax.h"
+
+int main(int argc, char* argv[]) {
+    // TODO: implement me!
+    std::runtime_error e("main() not implemented.");
+    throw e;
+}
