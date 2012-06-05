e4f2038c2e64d490ec1a4fca2bc8165aea165470
Joe Selman <selman.joe@gmail.com>
2012-06-05 16:13:36 -0700

    added more features to make it more compatible with std::map - not done yet

--- src/LRUCache.h
+++ src/LRUCache.h
@@ -13,24 +13,46 @@
 #include <iterator>
 #include <functional>
 
+template<typename K, typename V, typename C>
+class LRUCache; // forward declaration
+template<typename K, typename V, typename C, typename A>
+class LRUCacheIterator; // forward declaration
+
 template<typename K, typename V, typename C = std::less<K> >
 class LRUCache {
-public:
+private:
     typedef std::pair< K,V > ValuePair;
     typedef std::list< ValuePair > ValueList;
     typedef std::map< K, typename ValueList::iterator, C > ValueMap;
+public:
+    typedef K key_type;
+    typedef V mapped_type;
+    typedef std::pair<K, V> value_type;
+    typedef C key_compare;
 
-    LRUCache(unsigned int maxCapacity);
-    virtual ~LRUCache();
+    class value_compare : public std::binary_function<value_type, value_type, bool> {
+        friend class LRUCache<K, V, C>;
+    protected:
+        C comp_;
+        value_compare(C comp__) : comp_(comp__) {}
+    public:
+        bool operator()(const value_type& x, const value_type& y) {
+            return comp_(x.first, y.first);
+        }
+    };
 
-    virtual void insert(K key, V value);
-    virtual void insert(ValuePair pair);
-    virtual int count(K key) const {return map_.count(key);}
-    virtual int size() const {return vals_.size();}
-    virtual V get(K key);
-    virtual void clear();
-    virtual unsigned int capacity() const {return maxCapacity_;}
-    virtual void setCapacity(unsigned int maxCapacity);
+    typedef LRUCacheIterator<K, ValuePair, C, typename ValueMap::allocator_type> iterator;
+
+    LRUCache(unsigned int maxCapacity=1024);
+
+    void insert(K key, V value);
+    void insert(const std::pair<K, V>& pair);
+    int count(K key) const {return map_.count(key);}
+    int size() const {return vals_.size();}
+    V get(K key);
+    void clear();
+    unsigned int capacity() const {return maxCapacity_;}
+    void setCapacity(unsigned int maxCapacity);
 private:
     ValueList vals_;
     ValueMap map_;
@@ -44,17 +66,12 @@ LRUCache<K,V,C>::LRUCache(unsigned int maxCapacity)
 }
 
 template<typename K, typename V, typename C>
-LRUCache<K,V,C>::~LRUCache() {
-
-}
-
-template<typename K, typename V, typename C>
 void LRUCache<K,V,C>::insert(K key, V value) {
     insert(ValuePair(key, value));
 }
 
 template<typename K, typename V, typename C>
-void LRUCache<K,V,C>::insert(ValuePair pair) {
+void LRUCache<K,V,C>::insert(const std::pair<K, V>& pair) {
     if (maxCapacity_ == 0) return;
     // check to see if we have something already, if so overwrite
     K key = pair.first;
@@ -115,6 +132,62 @@ void LRUCache<K,V,C>::setCapacity(unsigned int maxCapacity) {
         map_.erase(key);
     }
 
+}
+
+namespace {
+template <class K, class V, class C, class A>
+class LRUCacheIterator : std::iterator<std::bidirectional_iterator_tag, V> {
+public:
+    LRUCacheIterator() : it_() {}
+    LRUCacheIterator(const LRUCacheIterator& i) : it_(i.it_) {}
+
+    LRUCacheIterator& operator=(const LRUCacheIterator& i) {
+        if (this != &i) {
+            it_ = i.it_;
+        }
+        return *this;
+    }
+
+    V& operator*() {
+        return *(it_->second);
+    }
+
+    V* operator->() {
+        return &(operator*());
+    }
+
+    LRUCacheIterator& operator++() {
+        it_++;
+        return *this;
+    }
+
+    LRUCacheIterator operator++(int) {
+        LRUCacheIterator itold(*this);
+        it_++;
+        return itold;
+    }
+
+    LRUCacheIterator& operator--() {
+        it_--;
+        return *this;
+    }
+
+    LRUCacheIterator operator--(int) {
+        LRUCacheIterator itold(*this);
+        it_--;
+        return itold;
+    }
+
+    bool operator==(const LRUCacheIterator& i) const{
+        return (it_ == i.it_);
+    }
+    bool operator!=(const LRUCacheIterator& i) const {return !operator==(i);}
+protected:
+    typedef typename std::map<K, typename std::list<V>::iterator, C, A >::iterator cache_it;
+    LRUCacheIterator(const cache_it& it) : it_(it) {}
+private:
+    cache_it it_;
+};
 }
 
 #endif /* LRUCACHE_H_ */
