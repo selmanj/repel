3286f81de64e6320701cfbac19680610b46be5a9
Joe Selman <selman.joe@gmail.com>
2012-06-27 13:56:09 -0700

    added maxwalksat test file as well as a single simple test to make sure simple steps are taken care of

--- test/MWSTest.cpp
+++ test/MWSTest.cpp
@@ -14,8 +14,27 @@
 #else
 #include <boost/test/included/unit_test.hpp>
 #endif
+#include <boost/random/mersenne_twister.hpp>
+#include "TestUtilities.h"
+#include "../src/inference/MaxWalkSat.h"
 
-BOOST_AUTO_TEST_CASE(MaxWalkSat) {
-    return; //  TODO add unit tests
+BOOST_AUTO_TEST_CASE(MaxWalkSat_simple) {
+    std::string facts(
+            "D-P(a) @ [1:10]\n"
+            "D-Q(a) @ [1:15]\n");
 
+    std::string formulas(
+            "1: [!D-P(a) v P(a)]\n"
+            "1: [!P(a) v D-P(a)]\n"
+            "1: [!D-Q(a) v Q(a)]\n"
+            "1: [!Q(a) v D-Q(a)]\n");
+
+    Domain d = loadDomainWithStreams(facts, formulas);
+    MWSSolver solver(&d);
+    boost::mt19937 rng(0);
+    Model m = solver.run(rng);
+    BOOST_CHECK_EQUAL(m.toString(), "D-P(a) @ {[1:10]}\n"
+            "D-Q(a) @ {[1:15]}\n"
+            "P(a) @ {[1:10]}\n"
+            "Q(a) @ {[1:15]}\n");
 }
