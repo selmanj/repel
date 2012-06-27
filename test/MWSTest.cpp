26c69d2f831d05034028d748f2f7b4fd97d2d6ab
Joe Selman <selman.joe@gmail.com>
2012-06-27 15:49:05 -0700

    added another simple test

--- test/MWSTest.cpp
+++ test/MWSTest.cpp
@@ -18,7 +18,7 @@
 #include "TestUtilities.h"
 #include "../src/inference/MaxWalkSat.h"
 
-BOOST_AUTO_TEST_CASE(MaxWalkSat_simple) {
+BOOST_AUTO_TEST_CASE( MaxWalkSat_simple ) {
     std::string facts(
             "D-P(a) @ [1:10]\n"
             "D-Q(a) @ [1:15]\n");
@@ -37,4 +37,26 @@ BOOST_AUTO_TEST_CASE(MaxWalkSat_simple) {
             "D-Q(a) @ {[1:15]}\n"
             "P(a) @ {[1:10]}\n"
             "Q(a) @ {[1:15]}\n");
+}
+
+BOOST_AUTO_TEST_CASE( MaxWalkSat_competing ) {
+    std::string facts(
+            "D-P(a) @ [1:10]\n"
+            "D-Q(a) @ [1:15]\n");
+
+    std::string formulas(
+            "5: [!D-P(a) v P(a)]\n"
+            "5: [!P(a) v D-P(a)]\n"
+            "1: [!D-Q(a) v Q(a)]\n"
+            "1: [!Q(a) v D-Q(a)]\n"
+            "5: [!P(a) v !Q(a)]");
+
+    Domain d = loadDomainWithStreams(facts, formulas);
+    MWSSolver solver(&d);
+    boost::mt19937 rng(0);
+    Model m = solver.run(rng);
+    BOOST_CHECK_EQUAL(m.toString(), "D-P(a) @ {[1:10]}\n"
+            "D-Q(a) @ {[1:15]}\n"
+            "P(a) @ {[1:10]}\n"
+            "Q(a) @ {[11:15]}\n");
 }
