79f7b89fca3a847fea385fd74828b85eb3b3b64b
Joe Selman <selman.joe@gmail.com>
2012-06-29 10:12:12 -0700

    adding latest version of stuff

--- datasets/recipe/forms.py
+++ datasets/recipe/forms.py
@@ -4,9 +4,11 @@ import sys
 
 forwardObsWeight = 1
 backwardObsWeight = 1
-defWeight = 10
+smallWeight = 1
+defWeight = 3
 
-obsPreds = ["OnTable", "Above", "Touched", "HandInFrame"]
+obsPredsWithObj= ["OnTable", "Above", "Touched"]
+obsPredsNoObj = ["HandInFrame"]
 
 def readObjects(filename):
     f = open(filename, 'r')
@@ -19,10 +21,13 @@ def readObjects(filename):
     return objs
 
 def printObsForms(objs):
-    for pred in obsPreds:
+    for pred in obsPredsWithObj:
         for obj in objs:
             print "{0}: [D-{1}({2}) -> {1}({2})]".format(forwardObsWeight, pred, obj)
             print "{0}: [{1}({2}) -> D-{1}({2})]".format(backwardObsWeight, pred, obj)
+    for pred in obsPredsNoObj:
+        print "{0}: [D-{1}() -> {1}()]".format(forwardObsWeight, pred)
+        print "{0}: [{1}() -> D-{1}()]".format(forwardObsWeight, pred)
 
 def am1(list, obj):
   preds = []
@@ -54,10 +59,10 @@ for obj1 in objs:
         l  = ""
         l += "{0}: "
         l += "!<>{{m}} [OnTable({1})] v "
-        l += "!<>{{=}} [!OnTable({1})] v "
+        l += "![!OnTable({1})] v "
         l += "!<>{{mi}} [OnTable({2})] v "
-        l += "!<>{{=}} [!OnTable({2})] v "
-        l += "!<>{{d}} [HandInFrame()]"
+        l += "![!OnTable({2})] v "
+        l += "<>{{d}} [!HandInFrame()]"
         r = ""
         r += "[InHand({1}, {2})]"
         print (l + " v " + r).format(defWeight, obj1, obj2)
@@ -81,5 +86,16 @@ for obj1 in objs:
             print "{0}: [!InHand({1}, {2})] v !<>{{*}} [InHand({3}, {2})]".format(defWeight, obj1, obj2, obj3)
 
 # TODO: bias an inhand so that other inhand events don't occur during
+
+print
+print "# Prefer inhand events where nothing changes on/off the table"
+for obj1 in objs:
+    for obj2 in objs:
+        if obj1 == obj2:
+            continue
+        for obj3 in objs:
+            if obj3 == obj1 or obj3 == obj2:
+                continue
+            print "{0}: !InHand({1}, {2}) v (!OnTable({3}) ; OnTable({3}))".format(smallWeight, obj1, obj2, obj3)
 # TODO: consider possibly adding rule preferring shorter inhands
 # Add Place/Deplace
